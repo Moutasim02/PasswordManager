37212a8d7777d792172b14723e2a9d10fbaa588c
Moutasim02 <moutasim9997@gmail.com>
2023-05-02 02:08:33 +0400

    Add visibility change onclick for the FloatingActionButton

--- app/src/main/java/com/archers/passwordmanager/MainActivity.java
+++ app/src/main/java/com/archers/passwordmanager/MainActivity.java
@@ -1,32 +1,60 @@
 package com.archers.passwordmanager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navigationView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        navigationView = findViewById(R.id.bottom_navigation);
+        addNavBarListener();
+    }
 
-        // Get the fragment manager
-        FragmentManager fragmentManager = getSupportFragmentManager();
+    private void addNavBarListener() {
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Fragment dynamicFragment = null;
 
-        // Begin a new transaction
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if (id == R.id.my_vault) {
+                    dynamicFragment = new VaultFragment();
+                    floatingActionButton.setVisibility(View.VISIBLE);
+                } else if (id == R.id.generator) {
+//                    dynamicFragment = new GeneratorFragment();
+                    floatingActionButton.setVisibility(View.INVISIBLE);
+                } else if (id == R.id.settings) {
+//                    dynamicFragment = new SettingsFragment();
+                    floatingActionButton.setVisibility(View.INVISIBLE);
+                } else if (id == R.id.profile) {
+//                    dynamicFragment = new ProfileFragment();
+                    floatingActionButton.setVisibility(View.INVISIBLE);
+                }
 
-        // Create a new instance of the fragment
-        VaultFragment myFragment = new VaultFragment();
-
-        // Add the fragment to the container
-        fragmentTransaction.add(R.id.fragment_container, myFragment);
-
-        // Commit the transaction
-        fragmentTransaction.commit();
+                if (dynamicFragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, dynamicFragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
     }
 }
