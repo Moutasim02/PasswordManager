0fad892c567958fba12be341fe756accbb72b8b1
Moutasim02 <moutasim9997@gmail.com>
2023-05-06 16:54:39 +0400

    Added new fragments and Activities

--- app/src/main/java/com/archers/passwordmanager/GeneratorFragment.java
+++ app/src/main/java/com/archers/passwordmanager/GeneratorFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link UserProfile#newInstance} factory method to
+ * Use the {@link GeneratorFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UserProfile extends Fragment {
+public class GeneratorFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class UserProfile extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public UserProfile() {
+    public GeneratorFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class UserProfile extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment UserProfile.
+     * @return A new instance of fragment Generator.
      */
     // TODO: Rename and change types and number of parameters
-    public static UserProfile newInstance(String param1, String param2) {
-        UserProfile fragment = new UserProfile();
+    public static GeneratorFragment newInstance(String param1, String param2) {
+        GeneratorFragment fragment = new GeneratorFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class UserProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        return inflater.inflate(R.layout.fragment_generator, container, false);
     }
 }
---
+++ app/src/main/java/com/archers/passwordmanager/OneTimePasswordActivity.java
@@ -0,0 +1,14 @@
+package com.archers.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OneTimePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_time_password);
+    }
+}
